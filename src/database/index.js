e81e54f464ca085d8b791dc16b12d0f93efcc451
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-09-04 16:56:44 -0300

    import do model File e criação do metodo associate

--- src/database/index.js
+++ src/database/index.js
@@ -1,19 +1,29 @@
 // fazer conexao com o bd e conectar com os models
 import Sequelize from 'sequelize';
 import User from '../app/models/User';
+import File from '../app/models/File';
 import configDB from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
     this.connection = new Sequelize(configDB);
     this.init();
+    this.associate();
   }
 
   init() {
     // Usando forEach por manipular os dados reais,
     models.forEach(model => model.init(this.connection));
+  }
+
+  associate() {
+    models.forEach(model => {
+      if (model.associate) {
+        model.associate(this.connection);
+      }
+    });
   }
 }
 
