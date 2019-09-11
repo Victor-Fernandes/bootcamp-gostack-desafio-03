90ca2261ffe470e112c43829667584278ab0f74a
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-09-11 01:05:22 -0300

    up

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -1,16 +1,16 @@
 /* eslint-disable class-methods-use-this */
-
+import User from '../models/User';
 import Meetup from '../models/Meetup';
 import Subscription from '../models/Subscription';
-import User from '../models/User';
 
 class SubscriptionController {
   async store(req, res) {
-    const user = await User.findByPk(req.UserId);
+    const user = await User.findByPk(req.userId);
     const meetup = await Meetup.findByPk(req.params.idMeetup, {
       include: [User],
     });
-
+    console.log(req.userId);
+    console.log(meetup.user_id);
     if (meetup.user_id === req.userId) {
       return res
         .status(400)
@@ -42,16 +42,12 @@ class SubscriptionController {
         .json({ error: "Can't subscribe to two meetups at the same time" });
     }
 
-    try {
-      const subscription = await Subscription.create({
-        user_id: user.id,
-        meetup_id: meetup.id,
-      });
-      return res.json(subscription);
-    } catch (error) {
-      return error;
-    }
+    const subscription = await Subscription.create({
+      user_id: user.id,
+      meetup_id: meetup.id,
+    });
+
+    return res.json(subscription);
   }
 }
-
 export default new SubscriptionController();
